bba24d3dcd58ce859f1b79d1b8f95cb024df6293
JLi2007 <LiJames9532@gmail.com>
2025-05-17 03:26:46 -0400

    changes to frontend! (semi-broken)

--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -1,6 +1,39 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  const [QR, setQR] = useState<any>(null);
+  const [netFile, setNetFile] = useState<any>(null);
+
+  // dummy
+  useEffect(() => {
+    fetchSP();
+  });
+
+  const fetchSP = async () => {
+    const res = await fetch("/dummy.sp");
+    const text = await res.text();
+    setNetFile(text);
+  };
+
+  const handleUpload = async (file: File) => {
+    const formData = new FormData();
+    formData.append("netlist", file);
+
+    const res = await fetch("/api/routes", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await res.json();
+    console.log(data);
+
+    // sets and shows qr code
+    setQR(data);
+  };
+
   return (
     <div className="relative min-h-screen w-screen tracking-wider">
       <div className="absolute inset-0 bg-violet-600/40 z-10 pointer-events-none" />
@@ -12,10 +45,10 @@ export default function Home() {
         <div className="w-full flex items-center justify-center">
           <div className="h-full flex mt-[20vh] w-[80%] flex-col text-violet-200 backdrop-blur-sm p-10 rounded-4xl items-center">
             <h1 className="text-5xl tracking-wider">
-              AR-powered real-time mobile app
+              AR-powered real-time circuit assembly app
             </h1>
             <div className="w-[50%]">
-              <h1 className="text-violet-100 text-xl">
+              <h1 className="text-violet-100 text-xl mt-5 text-center">
                 Guides users through breadboard circuit assembly by overlaying
                 step-by-step instructions directly on their hardware.
               </h1>
@@ -24,7 +57,14 @@ export default function Home() {
         </div>
 
         <div className="w-full flex items-center justify-center mt-10">
-          <button className="p-5 bg-violet-100/90 w-40 rounded-md cursor-pointer hover:bg-violet-100/80 transition-all duration-200 ease-in-out delay-100">upload json</button>
+          <button
+            onClick={() => handleUpload(netFile)}
+            className="p-5 bg-violet-100/80 w-40 rounded-md cursor-pointer hover:bg-violet-100/75 transition-all duration-200 ease-in-out delay-100"
+          >
+            upload json
+          </button>
+
+          {QR && <div>QR CODE</div>}
         </div>
       </div>
     </div>
